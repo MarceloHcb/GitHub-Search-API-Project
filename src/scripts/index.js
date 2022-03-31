9e963c787dabc042449b35076e73258bf6cfa981
MarceloHcb <marcelohcb@msn.com>
2022-03-31 15:43:20 -0300

    upgrade

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,11 @@ const screen = {
 
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
-        this.userProfile.innerHTML = `<hr><div class="info"><img src="${user.avatarUrl}" alt="Foto do Perfil do usuário />
+        this.userProfile.innerHTML = `<hr><div class="info"><img src="${user.avatarUrl}" alt="Foto do Perfil do usuário" />
                             <div class="data">
                                  <h1>${user.name ?? 'Não possui nome cadastrado 😥'}</h1>
                                  <p>${user.bio ?? 'Não possui bio cadastrada 😥'}</p>
+                                 </div>
                                 
                                  <div class="followers-content">
                                  <div class="followers">
